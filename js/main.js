6cbe7c7266a18f5ecd920517aded854234df1728
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-07-26 16:54:14 +0000

    Update element

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-console.log('Js work');
+// const titleElement = document.getElementById('title');
+
+// titleElement.textContent = "Utilisartion d'image";
+
+// const imgElement = document.querySelector('img');
+
+// imgElement.src = 'img/mailchimp2.jpg';
+
+// class="border blackWhite"
+
+// imgElement.classList.add('border');
+// imgElement.classList.add('blackWhite');
+
+// imgElement.classList.remove('blackWhite');
+
+// const bodyElement = document.querySelector('body');
+
+// bodyElement.style.backgroundColor = 'blue';
+// titleElement.style.color = 'white';
+
+// const divElement = document.querySelector('div');
+
+// divElement.innerHTML = `
+// <h2>Titre</h2>
+// <p>Un parageraph</p>
+// `;
